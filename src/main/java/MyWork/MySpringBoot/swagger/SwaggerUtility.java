88daf026c9f295857fa20227b891519d45549a69
Amit Kumar Pandey <amitkp311@gmail.com>
2024-02-05 13:16:57 +0530

    First cut

--- src/main/java/MyWork/MySpringBoot/swagger/SwaggerUtility.java
+++ src/main/java/MyWork/MySpringBoot/swagger/SwaggerUtility.java
@@ -26,4 +26,17 @@ public class SwaggerUtility {
                                 .build()));
     }
 
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("SpringShop API")
+                        .description("Spring shop sample application")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+
 }
